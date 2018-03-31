1c472c7bce16616c61ff3e4ea04b65f669535559
weiyu1990520@163.com <weiyu1990520@163.com>
2018-03-31 23:00:15 +0800

    规范代码结构

---
+++ spring-boot-core/src/main/java/com/weiyu/microService/chapter_1/Chapter1Controller.java
@@ -0,0 +1,19 @@
+package com.weiyu.microService.chapter_1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author weiyu1990520@163.com
+ * @Description
+ * @Date 22:41 2018/3/31
+ */
+@RestController
+@RequestMapping(value = "/chapter_1")
+public class Chapter1Controller {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(){
+        return "hello,it is successful";
+    }
+}
