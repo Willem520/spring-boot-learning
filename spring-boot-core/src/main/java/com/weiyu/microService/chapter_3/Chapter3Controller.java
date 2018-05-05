6bb00a6be3f8a124506f9e6aee64967394cae6db
weiyu1990520@163.com <weiyu1990520@163.com>
2018-05-05 17:33:11 +0800

    添加chapter_3项目代码

---
+++ spring-boot-core/src/main/java/com/weiyu/microService/chapter_3/Chapter3Controller.java
@@ -0,0 +1,25 @@
+package com.weiyu.microService.chapter_3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author weiyu1990520@163.com
+ * @Description
+ * @Date 15:01 2018/5/5
+ */
+@RestController
+@RequestMapping(value = "/chapter_3")
+public class Chapter3Controller {
+    @Value("${my.name}")
+    private String name;
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(){
+        return "hello,my name is "+name;
+    }
+}
